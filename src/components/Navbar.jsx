c518440ee303e631cff2f9b7446963f4e859f136
Roopaksh <72032743+Roopaksh1@users.noreply.github.com>
2023-09-06 02:09:36 +0530

    fix: fix logic for navbar menu
    - menu is not closing on clicking the button
    - navigation is not working when clicking on the link inside the menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,158 +1,164 @@
-import { useRef, useState } from "react";
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { useRef, useState } from 'react';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 
 const NavBar = () => {
   const [burger, setBurger] = useState(false);
-  const [userOp, setUSerOp] = useState(false);
+  const [userOp, setUserOp] = useState(false);
   const navbar = useRef();
   const userOptions = useRef();
-  const openUserOption = () => {
-    setUSerOp((prevState) => !prevState);
+  const toggleUserOption = () => {
+    setUserOp((prevState) => !prevState);
   };
-  const openBarsOption = () => {
+  const toggleBarsOption = () => {
     setBurger((prevState) => !prevState);
   };
   const CloseNavbar = (e) => {
-    if (
-      !userOptions.current.contains(e.target) ||
-      !navbar.current.contains(e.target)
-    ) {
+    if (userOp && !userOptions.current.contains(e.target)) {
+      setUserOp(false);
+    }
+
+    if (burger && !navbar.current.contains(e.target)) {
       setBurger(false);
-      setUSerOp(false);
     }
   };
 
   return (
     <div
-      className='overflow-hidden flex flex-col min-h-screen'
+      className="overflow-hidden flex flex-col min-h-screen"
       onMouseDown={CloseNavbar}
     >
-      <header className=' bg-navbar-bg p-4 flex justify-between text-white font-Roboto'>
+      <header className=" bg-navbar-bg p-4 flex justify-between text-white font-Roboto">
         <div>
-          <Link to='/'>
-            <div className='inline-block w-10 h-10 rounded-full text-center align-middle leading-9 overflow-hidden'>
-              <img src='favicon.ico' alt='logo' className='w-10 h-10' />
+          <Link to="/">
+            <div className="inline-block w-10 h-10 rounded-full text-center align-middle leading-9 overflow-hidden">
+              <img src="favicon.ico" alt="logo" className="w-10 h-10" />
             </div>
-            <span className='text-xl ml-2 font-semibold md:text-2xl'>
+            <span className="text-xl ml-2 font-semibold md:text-2xl">
               AI-Forms
             </span>
           </Link>
-          <div className='hidden md:inline-flex ml-10 text-lg font-light'>
+          <div className="hidden md:inline-flex ml-10 text-lg font-light">
             <NavLink
-              to={"/"}
+              to={'/'}
               className={({ isActive }) =>
                 isActive
-                  ? "border-b-2 border-active-link-color "
-                  : "hover:border-b-2 border-active-link-color"
+                  ? 'border-b-2 border-active-link-color '
+                  : 'hover:border-b-2 border-active-link-color'
               }
             >
               Home
             </NavLink>
             <NavLink
-              to={"/dashboard"}
+              to={'/dashboard'}
               className={({ isActive }) =>
                 isActive
-                  ? "border-b-2 border-active-link-color ml-4"
-                  : "ml-4 hover:border-b-2 border-active-link-color"
+                  ? 'border-b-2 border-active-link-color ml-4'
+                  : 'ml-4 hover:border-b-2 border-active-link-color'
               }
             >
               Dashboard
             </NavLink>
             <NavLink
-              to={"/about"}
+              to={'/about'}
               className={({ isActive }) =>
                 isActive
-                  ? "border-b-2 border-active-link-color ml-4"
-                  : "ml-4 hover:border-b-2 border-active-link-color"
+                  ? 'border-b-2 border-active-link-color ml-4'
+                  : 'ml-4 hover:border-b-2 border-active-link-color'
               }
             >
               About
             </NavLink>
             <NavLink
-              to={"/contact"}
+              to={'/contact'}
               className={({ isActive }) =>
                 isActive
-                  ? "border-b-2 border-active-link-color ml-4"
-                  : "ml-4 hover:border-b-2 border-active-link-color"
+                  ? 'border-b-2 border-active-link-color ml-4'
+                  : 'ml-4 hover:border-b-2 border-active-link-color'
               }
             >
               Contact
             </NavLink>
           </div>
         </div>
-        <div className='text-2xl'>
-          <button onClick={openBarsOption}>
-            <i className='fa-solid fa-bars md:hidden'></i>
-          </button>
-          <button
-            onClick={openUserOption}
-            className='ml-5 inline-block w-10 h-10 rounded-full bg-user-option-color text-center align-middle leading-4'
-          >
-            <i className='fa-solid fa-user'></i>
-          </button>
+        <div className="text-2xl">
+          <div className="inline-block" ref={navbar}>
+            <button onClick={toggleBarsOption}>
+              <i className="fa-solid fa-bars md:hidden"></i>
+            </button>
+            <div
+              className={
+                'z-90 absolute w-28 h-auto rounded-md top-16 right-12 float-right bg-slate-200 text-base' +
+                (burger ? '' : ' hidden')
+              }
+            >
+              <ul className="flex flex-col text-start p-2 text-slate-950">
+                <NavLink to={'/'} className={'my-1'} onClick={toggleBarsOption}>
+                  Home
+                </NavLink>
+                <NavLink
+                  to={'/dashboard'}
+                  className={'my-1'}
+                  onClick={toggleBarsOption}
+                >
+                  Dashboard
+                </NavLink>
+                <NavLink
+                  to={'/about'}
+                  className={'my-1'}
+                  onClick={toggleBarsOption}
+                >
+                  About
+                </NavLink>
+                <NavLink
+                  to={'/contact'}
+                  className={'my-1'}
+                  onClick={toggleBarsOption}
+                >
+                  Contact
+                </NavLink>
+              </ul>
+            </div>
+          </div>
+          <div ref={userOptions} className="inline-block">
+            <button
+              onClick={toggleUserOption}
+              className="ml-5 inline-block w-10 h-10 rounded-full bg-user-option-color text-center align-middle leading-4"
+            >
+              <i className="fa-solid fa-user"></i>
+            </button>
+            <div
+              className={
+                'z-90 absolute w-28 h-auto rounded-md top-16 right-4 float-right bg-slate-200 text-base' +
+                (userOp ? '' : ' hidden')
+              }
+            >
+              <ul className="flex flex-col text-center my-2 text-slate-950">
+                <NavLink
+                  to={'/'}
+                  className="p-1 cursor-pointer m-2"
+                  onClick={toggleUserOption}
+                >
+                  Your Profile
+                </NavLink>
+                <NavLink
+                  to={'/'}
+                  className="p-1 cursor-pointer m-2"
+                  onClick={toggleUserOption}
+                >
+                  Settings
+                </NavLink>
+                <NavLink
+                  to={'/'}
+                  className="p-1 cursor-pointer m-2"
+                  onClick={toggleUserOption}
+                >
+                  Sign Out
+                </NavLink>
+              </ul>
+            </div>
+          </div>
         </div>
       </header>
-      <div
-        ref={userOptions}
-        className={
-          "z-90 absolute w-28 h-auto rounded-md top-16 right-4 float-right bg-slate-200" +
-          (userOp ? "" : " hidden")
-        }
-      >
-        <ul className='flex flex-col text-center my-2 text-slate-950'>
-          <li className='p-1 cursor-pointer m-2'>Your Profile</li>
-          <li className='p-1 cursor-pointer m-2'>Settings</li>
-          <li className='p-1 cursor-pointer m-2'>Sign Out</li>
-        </ul>
-      </div>
-      <div
-        ref={navbar}
-        className={
-          "bg-slate-100 text-slate-800 w-full md:hidden flex flex-col items-center" +
-          (burger ? "" : " hidden")
-        }
-      >
-        <NavLink
-          to={"/"}
-          className={({ isActive }) =>
-            isActive
-              ? "border-b-2 border-navbar-bg my-1"
-              : " hover:scale-110 transition-transform my-1"
-          }
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to={"/dashboard"}
-          className={({ isActive }) =>
-            isActive
-              ? "border-b-2 border-navbar-bg my-1 "
-              : " hover:scale-110 transition-transform my-1"
-          }
-        >
-          Dashboard
-        </NavLink>
-        <NavLink
-          to={"/about"}
-          className={({ isActive }) =>
-            isActive
-              ? "border-b-2 border-navbar-bg my-1 "
-              : " hover:scale-110 transition-transform my-1"
-          }
-        >
-          About
-        </NavLink>
-        <NavLink
-          to={"/contact"}
-          className={({ isActive }) =>
-            isActive
-              ? "border-b-2 border-navbar-bg my-1"
-              : " hover:scale-110 transition-transform my-1"
-          }
-        >
-          Contact
-        </NavLink>
-      </div>
       <Outlet />
     </div>
   );
